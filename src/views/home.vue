c8a3857d6a1ab381e84f2ee6f251652e86916c73
YTQ <yangtaoqi0513@163.com>
2025-10-09 17:26:43 +0800

    first commit

---
+++ src/views/home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home-container">
+    <h1>Home</h1>
+    <Mobanyufa />
+  </div>
+</template>
+
+<script>
+import Mobanyufa from '../components/mobanyvfa.vue';
+export default {
+  name: 'Home',
+  components: {
+    Mobanyufa
+
+  }
+}
+</script>
+
+<style scroped>
+.home-container {
+  position: relative;
+  z-index: 10;
+}
+
+h1 {
+  z-index: 11;
+  color: aqua;
+}
+</style>
